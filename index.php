bf8c8bfb4e7be265688f930724a18591b06e500e
arulferianr <arulferianr@gmail.com>
2020-07-05 20:35:31 +0700

    menambahkan parafrag

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dignissimos explicabo sequi perspiciatis dolores debitis ab mollitia accusamus consequatur, aut fugit cum temporibus nulla facilis inventore eveniet, unde veniam deserunt?</p>
 </body>
 </html>
